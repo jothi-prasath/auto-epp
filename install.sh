9c9907c83fb5209c2cc5d9b303210c991669f4f0
Alexey336 <alexey200117@gmail.com>
2023-10-05 22:37:04 +0300

    Update install.sh
    On my system with Fedora 38 service won't start before you make script runnable

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo cp auto-epp /usr/bin/ -v
+chmod +x /usr/bin/auto-epp
 sudo cp auto-epp.service /etc/systemd/system/ -v
 sudo systemctl enable auto-epp
 sudo systemctl start auto-epp
